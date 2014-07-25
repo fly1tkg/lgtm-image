c8e2cab619011f879de5637e871529da069cd909
Shoichi Takagi <fly1tkg@gmail.com>
2014-07-25 10:19:35 +0900

    use composite method and composite LGTM image

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -2,13 +2,13 @@ class ApiController < ApplicationController
 
   def label
     image = MiniMagick::Image.open(params[:url])
-    image.combine_options do |i|
-      i.font 'Helvetica'
-      i.gravity 'SouthEast'
-      i.pointsize 44
-      i.fill 'white'
-      i.draw "text 20,20 'LGTM'"
+
+    lgtm = MiniMagick::Image.open("#{Rails.root}/app/assets/images/lgtm.png")
+    lgtm.resize "#{image['width'] / 2}x#{image['height'] / 2}"
+    result = image.composite(lgtm) do |c|
+      c.geometry '+20+20'
+      c.gravity 'SouthEast'
     end
-    render :text => image.to_blob, :content_type => 'image/jpg'
+    render :text => result.to_blob, :content_type => 'image/jpg'
   end
 end
